ce37e18fce92dd1fde93c5c5447653cdb37c37d1
Michael Clark <michaeljclark@mac.com>
2019-06-10 10:04:42 +1200

    test8: add test case for multi-channel signed distance field

--- tests/test8.cc
+++ tests/test8.cc
@@ -1,7 +1,22 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstdint>
+#include <climits>
+#include <cstring>
 
+#include <map>
+#include <vector>
+#include <memory>
 #include <string>
+#include <algorithm>
+
+#include "binpack.h"
+#include "utf8.h"
+#include "font.h"
+#include "glyph.h"
+#include "logger.h"
+#include "image.h"
+#include "file.h"
 
 #include "core/arithmetics.hpp"
 #include "core/Vector2.h"
@@ -22,33 +37,47 @@
 #include "ext/import-font.h"
 #include "msdfgen.h"
 
-const char *font_file = "fonts/Roboto-Regular.ttf";
+#include "core/edge-selectors.h"
+#include "core/contour-combiners.h"
 
-const char* shades[4][4] = {
+#include <ft2build.h>
+#include FT_FREETYPE_H
+#include FT_MODULE_H
+#include FT_GLYPH_H
+#include FT_OUTLINE_H
+
+typedef unsigned uint;
+
+static const char *font_file = "fonts/Roboto-Regular.ttf";
+
+static const char* shades[4][4] = {
     { " ", "▗", "▖", "▄" },
     { "▝", "▐", "▞", "▟" },
     { "▘", "▚", "▌", "▙" },
     { "▀", "▜", "▛", "█" },
 };
 
-std::string ansi_color(int r, int g, int b)
+static std::string ansi_color(int r, int g, int b)
 {
     char buf[32];
     snprintf(buf, sizeof(buf), "\x1B[38;2;%u;%u;%um", r, g, b);
     return std::string(buf);
 }
 
-void render_block(msdfgen::Bitmap<float, 3> &bitmap)
+static int rupeven(int n) { return (n + 1) & ~1; }
+
+static void render_block(msdfgen::Bitmap<float, 3> &bitmap, int w, int h)
 {
+    w = rupeven(w), h = rupeven(h);
     std::vector<std::string> v;
-    for (int y = 0; y < bitmap.height(); y += 2) {
+    for (int y = 0; y < h; y += 2) {
         std::string o;
-        for (int x = 0; x < bitmap.width(); x += 2) {
+        for (int x = 0; x < w; x += 2) {
             int c[2][2][3] = {0}, g[2][2] = {0}, s[3] = {0}, b[2][2] = {0};
             for (int u = 0; u < 2; u++) {
                 for (int v = 0; v < 2; v++) {
                     for (int w = 0; w < 3; w++) {
-                        int px = msdfgen::pixelFloatToByte(bitmap(x+u,y+v)[w]);
+                        int px = msdfgen::pixelFloatToByte(bitmap(x+u,h-y-v-1)[w]);
                         c[u][v][w] = px;
                         g[v][u] += c[u][v][w];
                         s[w] += c[u][v][w];
@@ -61,48 +90,100 @@ void render_block(msdfgen::Bitmap<float, 3> &bitmap)
         }
         printf("%s\n", o.c_str());
     }
+    printf("%s\n", ansi_color(255,255,255).c_str());
 }
 
-int main()
+static void copy_bitmap(image_ptr img, msdfgen::Bitmap<float, 3> &bitmap)
 {
-    msdfgen::Shape shape;
-    int codepoint = 'a';
-    double glyphAdvance = 0;
+    for (int y = 0; y < img->width; y++) {
+        uint8_t *row = img->getData() + y * img->width * 4;
+        for (int x = 0; x < img->height; x++) {
+            uint8_t *col = row + x * 4;
+            for (int w = 0; w < 3; w++) {
+                col[w] = msdfgen::pixelFloatToByte(bitmap(x,y)[w]);
+            }
+            col[3] = 0xff;
+        }
+    }
+}
 
-    msdfgen::FreetypeHandle *ft = msdfgen::initializeFreetype();
-    if (!ft) {
+int main(int argc, char **argv)
+{
+    int font_id = 0;
+    const char *file = nullptr;
+    bool console = false;
+
+    int dpi = 72;
+    int width = 64;
+    int height = 64;
+    unsigned codepoint = 'a';
+
+    bool overlapSupport = true;
+    bool scanlinePass = true;
+    double range = 1;
+    double angleThreshold = 3;
+    double edgeThreshold = 1.001;
+    double glyphAdvance = 0;
+    uint long long coloringSeed = 0;
+
+    msdfgen::Shape shape;
+    msdfgen::Bitmap<float, 3> msdf(width, height);
+    msdfgen::Vector2 translate, scale = { 3, 3 };
+
+    msdfgen::FreetypeHandle *ft = nullptr;
+    msdfgen::FontHandle *font = nullptr;
+
+    /*
+     * parse command line arguments
+     */
+
+    if (! ((argc == 3 && (strcmp(argv[1], "-file") == 0 && (file = argv[2]))) ||
+           (argc == 2 && (console = strcmp(argv[1], "-console") == 0))) ) {
+        fprintf(stderr, "usage: %s [-console|-file <output.png>]\n", argv[0]);
+        exit(1);
+    }
+
+    /*
+     * load font
+     */
+
+    if (!(ft = msdfgen::initializeFreetype())) {
         fprintf(stderr, "error: initializeFreetype failed\n");
         exit(1);
     }
-    msdfgen::FontHandle *font = msdfgen::loadFont(ft, font_file);
-    if (!font) {
+
+    if (!(font = msdfgen::loadFont(ft, font_file))) {
         msdfgen::deinitializeFreetype(ft);
         fprintf(stderr, "error: loadFont failed: filename=%s\n", font_file);
         exit(1);
     }
+
+    /*
+     * generate multi-channel SDF
+     */
+
     if (!msdfgen::loadGlyph(shape, font, codepoint, &glyphAdvance)) {
-        msdfgen::destroyFont(font);
         msdfgen::deinitializeFreetype(ft);
-        fprintf(stderr, "error: loadGlyph failed : codepoint=%d\n" , codepoint);
+        fprintf(stderr, "error: loadGlyph failed: codepoint=%d\n", codepoint);
         exit(1);
     }
-    msdfgen::destroyFont(font);
-    msdfgen::deinitializeFreetype(ft);
-
-    int width = 64, height = 64;
-    bool overlapSupport = true;
-    bool scanlinePass = true;
-    double range = 1;
-    msdfgen::Vector2 translate;
-    msdfgen::Vector2 scale = 3;
-    double angleThreshold = 3;
-    double edgeThreshold = 1.001;
-    unsigned long long coloringSeed = 0;
 
     msdfgen::edgeColoringSimple(shape, angleThreshold, coloringSeed);
-    msdfgen::Bitmap<float, 3> msdf(width, height);
-    shape.inverseYAxis = true;
     msdfgen::generateMSDF(msdf, shape, range, scale, translate,
         scanlinePass ? 0 : edgeThreshold, overlapSupport);
-    render_block(msdf);
+
+
+    if (console) {
+        render_block(msdf, width, height);
+    }
+
+    if (file) {
+        image_ptr img = image::createBitmap(msdf.width(), msdf.height(),
+            pixel_format_rgba);
+        copy_bitmap(img, msdf);
+        image::saveToFile(file, img);
+    }
+
+    msdfgen::destroyFont(font);
+    msdfgen::deinitializeFreetype(ft);
 }
