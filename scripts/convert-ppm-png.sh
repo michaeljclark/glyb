46bfc15f61e3e32926a6d2c48d0e16241e29968c
Michael Clark <michaeljclark@mac.com>
2019-06-23 17:56:20 +1200

    scripts: remove unused script that calls imagemagick

--- scripts/convert-ppm-png.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-video=build/video
-
-test -d ${video}/ppm || mkdir -p ${video}/ppm
-test -d ${video}/png || mkdir -p ${video}/png
-
-for f in ${video}/ppm/*ppm ; do convert -quality 100 $f ${video}/png/`basename $f ppm`png ; done
